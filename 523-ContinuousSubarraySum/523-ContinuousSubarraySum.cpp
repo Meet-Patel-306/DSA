f1102e97a7b2392582b29087a09bbe1ed176417f
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2025-08-18 18:37:39 +0530

    Create 523-ContinuousSubarraySum

---
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.cpp
@@ -0,0 +1,27 @@
+// Last updated: 18/08/2025, 18:37:43
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        map<int,int>mp;
+
+        mp[0] = -1;
+
+        int sum = 0, n = nums.size(), mod;
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            sum += nums[i];
+
+            mod = sum % k;
+
+            if(mp.find(mod) == mp.end()) mp[mod] = i;
+
+            else if((i - mp[mod]) > 1) return true;
+
+        }
+
+        return false;
+    }
+};
