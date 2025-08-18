42dabfd53fedf407f8b4c3ab6887e15c6db0aa53
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2025-08-18 18:35:56 +0530

    Create 1940-MaximumXorForEachQuery

---
+++ 1940-MaximumXorForEachQuery/1940-MaximumXorForEachQuery.cpp
@@ -0,0 +1,14 @@
+// Last updated: 18/08/2025, 18:36:00
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size(),xorr = nums[0],maxxorr = pow(2,maximumBit)-1;
+        for(int i=1;i<n;i++)xorr ^= nums[i];
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i] = xorr^maxxorr;
+            xorr ^= nums[n-1-i];
+        }
+        return ans;
+    }
+};
