0cd19e8c8420bf1f2ad8f8f17c77dfcf6387990a
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2025-08-18 18:37:02 +0530

    Create 988-FlipEquivalentBinaryTrees

---
+++ 988-FlipEquivalentBinaryTrees/988-FlipEquivalentBinaryTrees.cpp
@@ -0,0 +1,27 @@
+// Last updated: 18/08/2025, 18:37:06
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+ 
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        //end of tree check node1=node2
+        if(root1==NULL || root2==NULL)
+            return root1==root2;
+        //if any node1==node2 yes or no
+        if(root1->val!=root2->val)
+            return false;
+        //check all possible condition for two tree node
+        return (flipEquiv(root1->left,root2->left)&&flipEquiv(root1->right,root2->right))||(flipEquiv(root1->right,root2->left)&&flipEquiv(root1->left,root2->right));
+        
+    }
+};
