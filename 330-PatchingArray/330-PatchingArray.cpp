2d4008148b2db854ea58ed9c7215ee3dfcad33b7
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2025-08-18 18:38:02 +0530

    Create 330-PatchingArray

---
+++ 330-PatchingArray/330-PatchingArray.cpp
@@ -0,0 +1,31 @@
+// Last updated: 18/08/2025, 18:38:06
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long missing = 1;
+
+        int patches = 0;
+
+        int index = 0;
+
+        while (missing <= n) {
+
+            if (index < nums.size() && nums[index] <= missing) {
+
+                missing += nums[index];
+
+                index++;
+
+            } else {
+
+                missing += missing;
+
+                patches++;
+
+            }
+
+        }
+
+        return patches;
+    }
+};
