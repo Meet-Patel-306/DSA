290fd76e129b1e4e6b0098508b6ea5ae156d519d
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2025-08-18 18:36:37 +0530

    Create 1302-DeleteCharactersToMakeFancyString

---
+++ 1302-DeleteCharactersToMakeFancyString/1302-DeleteCharactersToMakeFancyString.cpp
@@ -0,0 +1,33 @@
+// Last updated: 18/08/2025, 18:36:41
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans = "";
+
+        ans.push_back(s[0]);
+
+        int n = s.size(),cnt = 1;
+
+        for(int i=1;i<n;i++){
+
+            if(s[i] == ans.back()){
+
+                cnt++;
+
+                if(cnt < 3)ans.push_back(s[i]);
+
+            }
+
+            else{
+
+                cnt = 1;
+
+                ans.push_back(s[i]);
+
+            }
+
+        }
+
+        return ans;
+    }
+};
