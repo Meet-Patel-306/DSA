36418aab51d7e750534fc83336abb7bcf749f85d
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2025-08-18 18:35:59 +0530

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.cpp
@@ -0,0 +1,9 @@
+// Last updated: 18/08/2025, 18:36:03
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+
+
+    }
+};
