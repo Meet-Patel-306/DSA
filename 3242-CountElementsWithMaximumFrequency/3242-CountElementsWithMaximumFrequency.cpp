b58e5e5cda0b7547dd101e51fc36c2b769094e4e
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2025-10-04 18:43:05 +0530

    Create 3242-CountElementsWithMaximumFrequency

---
+++ 3242-CountElementsWithMaximumFrequency/3242-CountElementsWithMaximumFrequency.cpp
@@ -0,0 +1,23 @@
+// Last updated: 04/10/2025, 18:43:06
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        int frequency = 0, count = 0;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (auto& pair : freq) {
+            if (pair.second == frequency) {
+                count++;
+            } else if (pair.second > frequency) {
+                frequency = pair.second;
+                count = 1;
+            }
+        }
+
+        return frequency * count;
+    }
+};
