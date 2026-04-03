35898d4c62040ffa6c1fbb2d97e68230e46a9821
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-04-03 16:00:13 +0530

    Create 3611-ConstructTheMinimumBitwiseArrayIi

---
+++ 3611-ConstructTheMinimumBitwiseArrayIi/3611-ConstructTheMinimumBitwiseArrayIi.cpp
@@ -0,0 +1,12 @@
+// Last updated: 03/04/2026, 16:00:17
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int n : nums) {
+            if(n != 2) ans.push_back(n - ((n + 1) & (-n - 1)) / 2);
+            else ans.push_back(-1);
+        }   
+        return ans;
+    }
+};
