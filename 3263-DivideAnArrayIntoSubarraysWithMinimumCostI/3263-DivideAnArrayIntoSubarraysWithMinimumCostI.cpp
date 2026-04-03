8b251dc53ce7cb385b6ac3da995e5ca191205192
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-04-03 16:00:57 +0530

    Create 3263-DivideAnArrayIntoSubarraysWithMinimumCostI

---
+++ 3263-DivideAnArrayIntoSubarraysWithMinimumCostI/3263-DivideAnArrayIntoSubarraysWithMinimumCostI.cpp
@@ -0,0 +1,18 @@
+// Last updated: 03/04/2026, 16:01:01
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int min1 = 100; 
+        int min2 = 100;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < min1){
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if(nums[i] < min2){
+                min2 = nums[i];
+            }
+        }
+        return nums[0] + min1 + min2;
+    }
+};
