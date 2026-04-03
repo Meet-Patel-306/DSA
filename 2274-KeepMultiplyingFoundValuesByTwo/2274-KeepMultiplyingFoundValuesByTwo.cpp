a673debac42d98bdb03881071add0af7318febb9
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-04-03 16:02:00 +0530

    Create 2274-KeepMultiplyingFoundValuesByTwo

---
+++ 2274-KeepMultiplyingFoundValuesByTwo/2274-KeepMultiplyingFoundValuesByTwo.cpp
@@ -0,0 +1,15 @@
+// Last updated: 03/04/2026, 16:02:04
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int k) {
+        int bits = 0;
+        for (auto& n : nums) {
+            if (n % k != 0) continue;
+            n = n / k;
+            if ((n & (n - 1)) == 0)
+                bits |= n;
+        }
+        int d = bits + 1;
+        return k * (d & -d);
+    }
+};
