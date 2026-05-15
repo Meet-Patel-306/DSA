cb478736234581414f163ff08f932a9d54f95434
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-05-15 17:43:12 +0530

    Create 1817-CalculateMoneyInLeetcodeBank

---
+++ 1817-CalculateMoneyInLeetcodeBank/1817-CalculateMoneyInLeetcodeBank.cpp
@@ -0,0 +1,19 @@
+// Last updated: 15/05/2026, 17:43:13
+class Solution {
+public:
+    int totalMoney(int n) {
+        int monday = 0, count = 0, sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(count == 7) {
+                monday = i/7 + 1;
+                count = 0;
+            } else {
+                monday++;
+            }
+            sum += monday;
+            count++;
+        }
+        return sum;
+    }
+};
