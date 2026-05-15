56c43fa1939efd934d0ad2e3022be7cc2cf6fceb
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-05-15 17:46:17 +0530

    Create 693-BinaryNumberWithAlternatingBits

---
+++ 693-BinaryNumberWithAlternatingBits/693-BinaryNumberWithAlternatingBits.cpp
@@ -0,0 +1,8 @@
+// Last updated: 15/05/2026, 17:46:18
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        unsigned int x = n ^ (n >> 1);
+        return (x & (x + 1)) == 0;
+    }
+};
