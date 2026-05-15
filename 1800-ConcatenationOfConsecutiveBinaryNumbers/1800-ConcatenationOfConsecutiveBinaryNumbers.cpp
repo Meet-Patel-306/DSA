ed79478a419db2aa1a22f18178cd387a72eeb8a0
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-05-15 17:43:17 +0530

    Create 1800-ConcatenationOfConsecutiveBinaryNumbers

---
+++ 1800-ConcatenationOfConsecutiveBinaryNumbers/1800-ConcatenationOfConsecutiveBinaryNumbers.cpp
@@ -0,0 +1,15 @@
+// Last updated: 15/05/2026, 17:43:18
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long res = 0;
+        int bits = 0;
+
+        for(int i=1; i<=n; i++){
+            if((i & (i-1)) == 0) bits++;
+            res = ((res<<bits) | i) % MOD;
+        }
+        return res;
+    }
+};
