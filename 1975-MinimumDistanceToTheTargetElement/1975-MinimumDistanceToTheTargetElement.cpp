c5cc786f7eba8646399c7007380de721e122324c
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-05-15 17:42:57 +0530

    Create 1975-MinimumDistanceToTheTargetElement

---
+++ 1975-MinimumDistanceToTheTargetElement/1975-MinimumDistanceToTheTargetElement.cpp
@@ -0,0 +1,12 @@
+// Last updated: 15/05/2026, 17:42:59
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int result = nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target)
+                result = min(result,abs(i-start));
+        }
+        return result;
+    }
+};
