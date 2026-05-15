54576b5a9daa1444bed7a6909236d7c25c8f40a7
MEET MANOJBHAI PATEL <meetpt306@gmail.com>
2026-05-15 17:43:46 +0530

    Create 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray

---
+++ 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray/1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 15/05/2026, 17:43:47
+class Solution {
+public:
+    static int minNumberOperations(vector<int>& target) {
+        adjacent_difference(target.begin(), target.end(), target.begin());
+        return accumulate(target.begin(), target.end(), 0,
+        [](int sum, int x){ return sum+=max(0,x);});
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
